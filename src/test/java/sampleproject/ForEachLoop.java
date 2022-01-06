72be845b053003f64375b6c2ff539905b9c5b589
sheereesh <alekhy99@gmail.com>
2022-01-06 23:43:00 +0530

    im a fool

---
+++ src/test/java/sampleproject/ForEachLoop.java
@@ -0,0 +1,14 @@
+package sampleproject;
+
+public class ForEachLoop {
+  	
+	public static void main(String[] args) {
+		int i[]= {1,2,3,4};
+	for(Object a:i)
+	{
+	System.out.println(a   );	
+	}
+		
+	}
+
+}
